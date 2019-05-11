4dac072c00931cb4fcec78c8f9bbedce846755c5
Molingejr <molingejr@gmail.com>
2019-05-11 04:35:33 +0100

    Completed password reset forms and link urls with console email

--- account/urls.py
+++ account/urls.py
@@ -1,5 +1,4 @@
 from django.urls import path
-from django.contrib.auth import views as auth_views
 from . import views
 
 """Register our all url paths for our app"""
@@ -9,16 +8,4 @@ urlpatterns = [
     path(r'signup', views.signup, name='signup'),
     path(r'login', views.log_in, name='log_in'),
     path(r'logout', views.user_logout, name='logout'),
-    path(r'password-reset/', auth_views.PasswordResetView.as_view(
-        template_name="account/password_reset.html"
-    ), name='password_reset'),
-    path(r'password-reset/done/', auth_views.PasswordResetDoneView.as_view(
-        template_name="account/password_reset_done.html"
-    ), name='password_reset_done'),
-    path(r'password-reset-confirm/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(
-        template_name="password_reset_confirm.html"
-    ), name='password_reset_confirm'),
-    path(r'password-reset-complete/', auth_views.PasswordResetCompleteView.as_view(
-        template_name="password_reset_complete.html"
-    ), name='password_reset_complete'),
 ]
--- config/urls.py
+++ config/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from django.contrib.auth import views as auth_views
 import feedback.urls
 import account.urls
 
@@ -27,5 +28,16 @@ urlpatterns = [
     path('', include(
             account.urls, namespace='account'
     )),
-    path(r'^', include('django.contrib.auth.urls'))
+    path(r'password-reset/', auth_views.PasswordResetView.as_view(
+        template_name="password_reset.html"
+    ), name='password_reset'),
+    path(r'password-reset/done/', auth_views.PasswordResetDoneView.as_view(
+        template_name="password_reset_done.html"
+    ), name='password_reset_done'),
+    path(r'password-reset-confirm/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(
+        template_name="password_reset_confirm.html"
+    ), name='password_reset_confirm'),
+    path(r'password-reset-complete/', auth_views.PasswordResetCompleteView.as_view(
+        template_name="password_reset_complete.html"
+    ), name='password_reset_complete'),
 ]
